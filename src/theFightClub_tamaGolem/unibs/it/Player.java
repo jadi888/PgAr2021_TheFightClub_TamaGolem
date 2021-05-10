e8c26b44dd28db38821aa52b092f3071e3d9d6f8
souhailamouak <s.mouak@studenti.unibs.it>
2021-05-10 14:00:36 +0200

    Classe player da modificare ( devo aggiustare ancora un po' di cose)

--- src/theFightClub_tamaGolem/unibs/it/Player.java
+++ src/theFightClub_tamaGolem/unibs/it/Player.java
@@ -1,40 +1,69 @@
 package theFightClub_tamaGolem.unibs.it;
 
+import java.io.*;
 import java.util.*;
+  public class Player {
 
-public class Player {
+    //metto gli attributi della classe
+    private String nome; // nome del giocatore
+    private int numeroGolem; //numero di golem che ha a disposizione
+    private Golem golemScelto;
+    private static Golem golem = new Golem();
+    private static ScortaPietre pietra = new ScortaPietre();
+    private int numeroPietre;
 
 
-    private static String nomeGiocatore;
-    private Golem golemInGioco;
+    // -> G è il numero di golem
+    static int G = (int) Math.ceil(((golem.getVitaGolem()-1)*(golem.getVitaGolem()-2))/(2*golem.getP()));
+    private static ArrayList<Golem> golemTotali = new ArrayList<>();
+    //Array list per i golem
 
-    int G = (int) Math.ceil(((golem.getVitaGolem()-1)*(golem.getVitaGolem()-2))/(2*golem.getP())); //numero di golem per giocatore
-    private static ArrayList<Golem> golems = new ArrayList<>(); // un giocatore ha a disposizione una quantità G di Golem;
-
-    public void PLayer(){
+    public void Giocatore (){
         this.G = G;
-        this.nomeGiocatore = nomeGiocatore;
+        this.nome = nome;
     }
 
 
 
-    //quando il giocatore evoca un Golem, il golem automaticamente viene tolto dall'arraylist golems
-    // e viene dimunuito il numero di golems a disposizione del giocatore
-    public boolean evocaGolem(){
+    //Devo prendere i golem dall'array list di tamagolem che ho a disposizione
+    public boolean evocazioneGolem() {
+        System.out.println("Benvenuto Giocatore, adesso puoi evocare il tuo GOLEM!" );
         int i=0;
-        if (G>0){
-            System.out.println("Il giocatore " + nomeGiocatore + " ha evocato un GOLEM!");
-            golem = golems.get(i);
-            G--;
-            golems.remove(i);
+        if (G>0) {
+            golem = golemTotali.get(i);
+            System.out.println("Adesso hai evocato il tuo primo golem"+ golem );
+            G = G-1;
+            golemTotali.remove(i);
             return true;
         }
-        else return false;
+
+        System.out.println("Non hai più golem a disposizione");
+        return false;
+
     }
 
-   public void setNomeGiocatore(String nome){
-        this.nomeGiocatore = nome;
-   }
+    //SCELTA DEL SET DI PIETRE CHE OGNI GIOCATORE HA A DISPOSIZIONE PER IL GIOCO
+    //QUESTO SARA' IL SET CHE OGNI GIOCATORE DOVRA' USARE PER L'INTERA PARTIT
+    //DOVRA' BASTARE AL GIOCATORE PER TUTTI I GOLEM IN GIOCO
 
 
+    int S = ((2*golem.getP()* Player.G)/ golem.getVitaGolem())* golem.getVitaGolem();
+    public static ArrayList<ScortaPietre> scortaPietre = new ArrayList<>();
+
+    public boolean caricamentoPietre()
+    {
+        int j=0;
+
+        if (S > 0)
+        {
+            do {
+                pietra = scortaPietre.get(j);
+                S--;
+                numeroPietre--;
+                scortaPietre.remove(j);
+            } while (numeroPietre > 0 && S > 0);
+            return true;
+        }
+        return false;
+    }
 }
